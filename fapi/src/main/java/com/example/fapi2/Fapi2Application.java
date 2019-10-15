88534a133e902a694eddcc2c7cdaa69da631193a
dimas <dimasemin.98@gmail.com>
2019-10-15 20:11:52 +0300

    project structure adding all stuff

---
+++ fapi/src/main/java/com/example/fapi2/Fapi2Application.java
@@ -0,0 +1,13 @@
+package com.example.fapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fapi2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fapi2Application.class, args);
+    }
+
+}
